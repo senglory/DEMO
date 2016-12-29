8cdfeba2bb969643c164b03933fb30c9ca9295c5
Nikolay Sonin <senglory@gmail.com>
2016-12-29 20:25:40 +0300

    initial commit

---
+++ TestUseTech/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.UserProfiles;
+
+
+
+namespace TestUseTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user login (domain\\user):");
+            var un = "test\\Uppre1932";// Console.ReadLine();
+            if (!string .IsNullOrEmpty(un))
+            {
+                try
+                {
+                    var unExact = un.IndexOf("\\") > 0 ? un.Substring(un.IndexOf("\\") + 1) : un;
+                    #region URL magic
+                    Uri uri = new Uri(Properties.Settings.Default.ListUrl);
+                    var idxLst = uri.Segments.ToList().IndexOf("Lists/");
+                    if (idxLst == -1 || idxLst == uri.Segments.Length - 1)
+                        return;
+                    var url = uri.Scheme + "://" + uri.Host + ":" + uri.Port;
+                    for (var i = 0; i < idxLst; i++)
+                        url += uri.Segments[i];
+                    var urlLst = uri.Segments[idxLst] + uri.Segments[idxLst + 1];
+                    #endregion
+
+                    using (ClientContext ctx = new ClientContext(url))
+                    {
+                        #region Get info about user by his login
+                        //PeopleManager peopleManager = new PeopleManager(ctx);
+                        //PersonProperties personProperties = peopleManager.GetPropertiesFor(un);
+                        //ctx.Load(personProperties, p => p.AccountName,  p => p.UserProfileProperties);
+                        //ctx.ExecuteQuery();
+                        //foreach (var property in personProperties.UserProfileProperties)
+                        //{
+                        //    Debug .WriteLine(string.Format("{0}: {1}", property.Key.ToString(), property.Value.ToString()));
+                        //}
+
+
+
+                        List siteUserInfoList = ctx.Web.SiteUserInfoList;
+                        CamlQuery query2 = new CamlQuery();
+                        query2.ViewXml = @"<View  Scope='RecursiveAll'>
+<Query>
+<Where>
+    <Eq>
+      <FieldRef Name='UserName'  />
+      <Value Type='Text'>"
+    + unExact +
+          @"</Value>
+    </Eq>
+</Where>
+</Query></View>";
+                        IEnumerable<ListItem> itemColl = ctx.LoadQuery(siteUserInfoList.GetItems(query2));
+                        ctx.ExecuteQuery();
+                        ListItem theUser = null;
+                        foreach (var li in itemColl)
+                        {
+                            theUser = li;
+                            break;
+                            //Debug.WriteLine("ID:{0}  Login:{2}\\{1} Title:{3}", li.Id, li["UserName"], "",li["Title"]);
+                        }
+
+                        #endregion
+
+                        if (theUser != null)
+                        {
+                            #region Do task search
+                            List tasksList;
+                            ListItemCollection taskItems;
+
+
+                            tasksList = ctx.Web.LoadListByUrl(urlLst);
+                            CamlQuery query = new CamlQuery();
+                            query.ViewXml = @"<View  Scope='RecursiveAll'><Query>
+<Where>
+<And>
+    <Lt>
+      <FieldRef Name='DueDate'  />
+      <Value Type='DateTime'><Today /></Value>
+    </Lt>
+    <Includes> 
+        <FieldRef Name='AssignedTo' LookupId='TRUE'/> 
+        <Value Type='Integer'>"
+    + theUser.Id +
+    @"</Value>
+    </Includes>
+</And>
+</Where>
+</Query></View>";
+                            taskItems = tasksList.GetItems(query);
+                            ctx.Load(taskItems);
+                            ctx.ExecuteQuery();
+                            foreach (var li in taskItems)
+                            {
+                                Console.WriteLine("ID: {0} \nTitle: {1}", li.Id, li["Title"]);
+                            }
+                            #endregion
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message );
+                }
+            }
+        }
+    }
+}
---
+++ TestUseTech/WebExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+
+namespace TestUseTech
+{
+    static class WebExtensions
+    {
+        /// <summary>
+        /// Load List by server relative Url
+        /// </summary>
+        /// <param name="web"></param>
+        /// <param name="listUrl"></param>
+        /// <returns></returns>
+        public static List LoadListByUrl(this Web web, string listUrl)
+        {
+            var ctx = web.Context;
+            var listFolder = web.GetFolderByServerRelativeUrl(listUrl);
+            ctx.Load(listFolder.Properties);
+            ctx.ExecuteQuery();
+            var listId = new Guid(listFolder.Properties["vti_listname"].ToString());
+            var list = web.Lists.GetById(listId);
+            ctx.Load(list);
+            ctx.ExecuteQuery();
+            return list;
+        }
+
+        /// <summary>
+        /// Resolve client context  
+        /// </summary>
+        /// <param name="requestUri"></param>
+        /// <param name="context"></param>
+        /// <param name="credentials"></param>
+        /// <returns></returns>
+        public static bool TryResolveClientContext(Uri requestUri, out ClientContext context )
+        {
+            context = null;
+            var baseUrl = requestUri.GetLeftPart(UriPartial.Authority);
+            for (int i = requestUri.Segments.Length; i >= 0; i--)
+            {
+                var path = string.Join(string.Empty, requestUri.Segments.Take(i));
+                string url = string.Format("{0}{1}", baseUrl, path);
+                try
+                {
+                    context = new ClientContext(url);
+                    context.ExecuteQuery();
+                    return true;
+                }
+                catch (Exception ex) { }
+            }
+            return false;
+        }
+    }
+}
